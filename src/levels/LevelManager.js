bbf836ccaef96cb455df9b32ebd01ad0e198b052
riko781 <henrickcarneva@gmail.com>
2026-06-28 23:08:59 +0200

    create level manager

---
+++ src/levels/LevelManager.js
@@ -0,0 +1,25 @@
+import { CreateLevel1 } from "./levels/Level1";
+import { CreateLevel2 } from "./levels/Level2";
+
+let currentLevel = 0;
+export let level;
+
+const levels = [
+    CreateLevel1,
+    CreateLevel2,
+];
+
+export function nextLevel(scene){
+    currentLevel++;
+
+    if(currentLevel >= levels.length){
+        console.log("GAME FINISHED");
+        currentLevel = 0 ;
+    }
+
+    scene.scene.restart();
+}
+
+export function loadCurrentLevel(scene,player,enemy){
+    level = levels[currentLevel](scene,player,enemy);
+}
