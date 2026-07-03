a6b2417de80d3bc9d2538af6de5dd147d20f3209
riko781 <henrickcarneva@gmail.com>
2026-07-03 17:02:42 +0200

    refactor: update enemy and platform configurations in Level2.js for improved gameplay

--- src/levels/Level2.js
+++ src/levels/Level2.js
@@ -1,4 +1,3 @@
-import { MovingPlatform } from '../MovingPlateform.js';
 import { JUMP_FORCE } from '../core/GameplayConstants.js';
 import { Enemy } from '../Enemy';
 
@@ -7,59 +6,41 @@ export function CreateLevel2(scene,player) {
     const enemies = [];
 
     enemies.push(
-        new Enemy(scene, 1450, 380,1380, 1520)
+        new Enemy(scene, 750, 200 ,650, 800),
+        new Enemy(scene, 850, 200 ,870, 1030)
     );
-
     const enemy = enemies[0];
-
+    const enemy2 = enemies[1];
     //platforms
     const rectangle = scene.add.rectangle(200, 0, 10, 10, 0x00ff00);
-    const beach_water = scene.add.rectangle(1350, 600, 2000, 50, 0x87CEEB);
-    const dangerStart = scene.add.rectangle(1300,430,120,20,0x632800);
-    const goal = scene.add.rectangle(1800,350,60,120,0x00ff00);
-    const enemyPlatform = scene.add.rectangle(1450,430,220,20,0x632800);
+    const goal = scene.add.rectangle(1080,450,60,300,0x00ff00);
     const solPlatform = scene.add.rectangle(175, 600, 348, 100, 0x632800);
     const exitPlatform = scene.add.rectangle(1650,430,120,20,0x632800);
     
-    const plateForm_1 = scene.add.rectangle(450, 500, 200, 20, 0x632800);
-    const plateForm_2 = scene.add.rectangle(650, 440, 200, 20, 0x632800);
-    const plateForm_3 = scene.add.rectangle(850, 450, 50, 20, 0x632800);
-    const plateForm_4 = scene.add.rectangle(950, 450, 50, 20, 0x632800);
-    
-    //moving platform
-    const movingPlatform = new MovingPlatform(scene, 1100, 400, 100, 20, 50, 50);
+    const plateForm_1 = scene.add.rectangle(449, 550, 200, 100, 0x632800);
+    const enemyPlatform = scene.add.rectangle(649, 525, 400, 150, 0x632800);
+    const plateForm_3 = scene.add.rectangle(849, 550, 400, 100, 0x632800);
     
     //physics
-    scene.physics.add.existing(beach_water,true);
     scene.physics.add.existing(rectangle);
-    scene.physics.add.existing(dangerStart,true);
     scene.physics.add.existing(enemyPlatform,true);
     scene.physics.add.existing(exitPlatform,true);
     scene.physics.add.existing(goal,true);
     scene.physics.add.existing(solPlatform,true);
     scene.physics.add.existing(plateForm_1,true);
-    scene.physics.add.existing(plateForm_2,true);
     scene.physics.add.existing(plateForm_3,true);
-    scene.physics.add.existing(plateForm_4,true);
 
     //colliders  
     scene.physics.add.collider(rectangle,solPlatform);
-    scene.physics.add.collider(player.sprite,dangerStart);
     scene.physics.add.collider(player.sprite,exitPlatform);
     scene.physics.add.collider(player.sprite,solPlatform);
     scene.physics.add.collider(player.sprite,plateForm_1);
-    scene.physics.add.collider(player.sprite,plateForm_2);
+    scene.physics.add.collider(player.sprite,plateForm_3);
+    scene.physics.add.collider(enemy2.sprite,plateForm_3);
     scene.physics.add.collider(player.sprite,enemyPlatform);
     scene.physics.add.collider(enemy.sprite,enemyPlatform);
-    scene.physics.add.collider(player.sprite,plateForm_3);
-    scene.physics.add.collider(player.sprite,plateForm_4);
-    scene.physics.add.collider(player.sprite,movingPlatform.platform);
 
     //overlap
-    scene.physics.add.overlap(player.sprite, beach_water, () => {
-        player.kill();
-    });
-
     scene.physics.add.overlap(
         player.sprite,
         enemy.sprite,
@@ -81,6 +62,25 @@ export function CreateLevel2(scene,player) {
 
     scene.physics.add.overlap(
         player.sprite,
+        enemy2.sprite,
+        () => {
+            // si le joueur tombe dessus
+            if (player.sprite.body.velocity.y > 0
+                && player.sprite.y < enemy2.sprite.y) {
+
+                enemy2.kill();
+
+                player.sprite.body.velocity.y = JUMP_FORCE * 0.6;
+
+            } else {
+
+                player.kill();
+            }
+        }
+    );
+
+    scene.physics.add.overlap(
+        player.sprite,
         goal,
         () => {
             console.log("YOU WIN");
@@ -89,8 +89,6 @@ export function CreateLevel2(scene,player) {
     );
 
     return {
-        enemies,
-        movingPlatform: movingPlatform
+        enemies
     }
-
 }
