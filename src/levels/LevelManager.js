47b11231b2557c95a31bad3a545a16b830f52186
riko781 <henrickcarneva@gmail.com>
2026-07-03 17:04:03 +0200

    refactor: add CreateLevel3 import to LevelManager.js for level management

--- src/levels/LevelManager.js
+++ src/levels/LevelManager.js
@@ -1,5 +1,6 @@
 import { CreateLevel1 } from "./Level1";
 import { CreateLevel2 } from "./Level2";
+import { CreateLevel3 } from "./Level3";
 
 let currentLevel = 0;
 export let level;
@@ -7,6 +8,7 @@ export let level;
 const levels = [
     CreateLevel1,
     CreateLevel2,
+    CreateLevel3
 ];
 
 export function nextLevel(scene){
