ef731a976a55cb1250f9ca0ae350b79a2922a8a3
riko781 <henrickcarneva@gmail.com>
2026-08-06 23:01:48 +0200

    feat: load enemy sprite sheet and update enemy movement logic

--- src/main.js
+++ src/main.js
@@ -43,7 +43,9 @@ let enemy;
 function preload() {
     const basePath = `${import.meta.env.BASE_URL}assets`;
     const playerPath = `${basePath}/player-Sheet.png`;
+    const enemyPath = `${basePath}/enemy-Sheet.png`;
     this.load.spritesheet('player', playerPath, { frameWidth: 64, frameHeight: 64 });
+    this.load.spritesheet('enemy', enemyPath, { frameWidth: 24, frameHeight: 24 });
 
     this.load.image('tiles', `${basePath}/tiles/tilemap_packed.png`);
     this.load.image('tile_0004', `${basePath}/tiles/tile_0004.png`);
@@ -72,7 +74,7 @@ function create() {
 function update(timer, delta) {
     level?.movingPlatform?.update?.();
     player.update(delta);
-    level.enemies?.forEach(enemy => enemy.update());
+    level.enemies?.forEach(enemy => enemy.update(delta));
     level.movingPlatforms?.forEach(platform => platform.update());
 
     const cam = this.cameras.main;
